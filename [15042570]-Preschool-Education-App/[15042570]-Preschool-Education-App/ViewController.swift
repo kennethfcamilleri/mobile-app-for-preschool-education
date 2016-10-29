d0792b97530019846b0148d99df3fa8c44398fc6
Kenneth Forseth Camilleri <kenneth.forseth@gmail.com>
2016-10-29 09:46:48 +0200

    Add files via upload

---
+++ [15042570]-Preschool-Education-App/[15042570]-Preschool-Education-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  [15042570]-Preschool-Education-App
+//
+//  Created by Kenneth F. Camilleri on 28/10/2016.
+//  Copyright © 2016 Kenneth Forseth Camilleri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
